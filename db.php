dc315fc816a46ee51144d1beb46f6d26f4f29c7f
ThasaneeBoontiam <6306021422071@fitm.kmutnb.ac.th>
2021-03-09 12:06:48 +0700

    create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+    define("hostname","localhost");
+    define("user","user");
+    define("password","azagP81g5eJxdb7i");
+    define("dbname","bookstore");
+?>
+
+<?php
+    $mysqli= new mysqli(hostname,user,password,dbname);
+    //check connection
+    if
+?>

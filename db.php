46d2e91e6492c6b379d20e64863e94cbb586606c
ThasaneeBoontiam <6306021422071@fitm.kmutnb.ac.th>
2021-03-11 13:46:30 +0700

    dddd

--- db.php
+++ db.php
@@ -1,12 +1,18 @@
 <?php
     define("hostname","localhost");
     define("user","user");
-    define("password","azagP81g5eJxdb7i");
+    define("password", "azagP81g5eJxdb7i");
     define("dbname","bookstore");
 ?>
 
 <?php
-    $mysqli= new mysqli(hostname,user,password,dbname);
-    //check connection
-    if
+    $mysqli = new mysqli(hostname, user, password, dbname);
+    // Check connection
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        exit();
+    }else{
+        echo "Connect successgg...";
+    }
+    $sqltxt = ""
 ?>

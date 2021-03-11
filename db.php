601ecaf87b75eedf0b488b0c069e73fe19669139
ThasaneeBoontiam <6306021422071@fitm.kmutnb.ac.th>
2021-03-11 15:45:42 +0700

    ccc

--- db.php
+++ db.php
@@ -1,20 +1,53 @@
 <?php
+class Database{
+    public $conn = null;
+    public function connect() //เชื่อมต่อฐานข้อมูล
+    {
     define("hostname","localhost");
     define("user","user");
     define("password", "azagP81g5eJxdb7i");
     define("dbname","bookstore");
-?>
-
-<?php
-    $mysqli = new mysqli(hostname, user, password, dbname);
-    $mysqli->query("SET NAMES UTF8");
+    $this->conn = new mysqli(hostname, user, password, dbname);
+    $this->conn->query("SET NAMES UTF8");
     // Check connection
-    if ($mysqli -> connect_errno) {
-        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    if ($this->conn -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $conn -> connect_error;
         exit();
     }else{
         echo "Connect successgg...";
     }
+    }
     
+    public function showBook(){
+        //แสดงข้อมูลใน table
+        $sql = "SELECT * FROM `book`";
+        $result = $this->conn->query($sql);
+        ///echo "Book Store";
+        echo "<table border='1'>";
+        $counter = 0;
+        while ($row = $result->fetch_assoc()) {
+            if ($counter == 0) {
+                echo "<tr>";
+                foreach ($row as $key => $value) {
+                    echo "<th>{$key}</th>";
+                }
+                
+                echo "</tr>";
+                $counter++;
+            }
+            echo "<tr>";
+            foreach ($row as $key => $value) {
+                echo "<td>{$value}</td>";
+            }
+            
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+
+    public function disconnect(){
+        $this->conn->close();
+    }
+}
 ?>
 
--- show_data.php
+++ show_data.php
@@ -1,26 +1,7 @@
 <?php
-    include "db.php";
-        //แสดงข้อมูลใน table
-        $sql = "SELECT * FROM `book`";
-        $result = $mysqli->query($sql);
-        echo "<table border='1'>";
-        $counter = 0;
-        while ($row = $result->fetch_assoc()) {
-            if ($counter == 0) {
-                echo "<tr>";
-                foreach ($row as $key => $value) {
-                    echo "<th>{$key}</th>";
-                }
-                
-                echo "</tr>";
-                $counter++;
-            }
-            echo "<tr>";
-            foreach ($row as $key => $value) {
-                echo "<td>{$value}</td>";
-            }
-            
-            echo "</tr>";
-        }
-        echo "</table>";
+        include_once "db.php";
+        $myconn = new Database();
+        $myconn->connect();
+        $myconn->showBook();
+        $myconn->disconnect();
         ?>

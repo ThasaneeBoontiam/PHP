65e3559e96030c562cdaa44a2852705c5439fa51
ThasaneeBoontiam <6306021422071@fitm.kmutnb.ac.th>
2021-03-11 14:57:18 +0700

    cc

--- db.php
+++ db.php
@@ -18,28 +18,3 @@
     
 ?>
 
-<?php
-        //แสดงข้อมูลใน table
-        $sql = "SELECT * FROM `book`";
-        $result = $mysqli->query($sql);
-        echo "<table border='1'>";
-        $counter = 0;
-        while ($row = $result->fetch_assoc()) {
-            if ($counter == 0) {
-                echo "<tr>";
-                foreach ($row as $key => $value) {
-                    echo "<th>{$key}</th>";
-                }
-                
-                echo "</tr>";
-                $counter++;
-            }
-            echo "<tr>";
-            foreach ($row as $key => $value) {
-                echo "<td>{$value}</td>";
-            }
-            
-            echo "</tr>";
-        }
-        echo "</table>";
-        ?>

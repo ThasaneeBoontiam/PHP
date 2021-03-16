2b9cc5a93c42f120556eb65e6e7b246b10b5d969
ThasaneeBoontiam <6306021422071@fitm.kmutnb.ac.th>
2021-03-16 09:58:08 +0700

    ccc

---
+++ json_import/movie.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $jsonfile = file_get_contents("movies.json");
+    ?>
+    <select id="year_movie">
+    </select>
+
+    <script>
+        var str = "";
+        var jsonEx = <?php echo $jsonfile;?>;
+    </script>
+    <div id="output"></div>
+</body>
+</html>
